890b7e25fdbcf174920150eada9a8e882ecc7b68
Ivan <kulkov_2003@bk.ru>
2024-01-31 14:13:14 +0300

    Add Radical model and migration

---
+++ database/migrations/2024_01_31_111004_create_radicals_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('radicals', function (Blueprint $table) {
+            $table->id();
+            $table->string('character');
+            $table->string('meaning');
+            $table->string('image');
+            $table->unsignedTinyInteger('level');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('radicals');
+    }
+};
