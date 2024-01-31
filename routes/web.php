c9c4fe81fe88a88a8e7e0789e0f9aca837f40128
Ivan <kulkov_2003@bk.ru>
2024-01-31 16:00:53 +0300

    Creating routes structure

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return Inertia::render('Main');
@@ -22,11 +12,23 @@ Route::get('/', function () {
 
 Route::get('/kanjis', function () {
     return Inertia::render('Kanjis');
-})->name('kanjis');
+})->name('kanjis.index');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/kanjis/{kanji}', function () {
+    return Inertia::render('KanjisShow');
+})->name('kanjis.show');
+
+Route::get('/radicals', function () {
+    return Inertia::render('Radicals');
+})->name('radicals.index');
+
+Route::get('/radicals/{radical}', function () {
+    return Inertia::render('RadicalsShow');
+})->name('radicals.index');
+
+//Route::get('/dashboard', function () {
+//    return Inertia::render('Dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
 
 //Route::middleware('auth')->group(function () {
 //    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
