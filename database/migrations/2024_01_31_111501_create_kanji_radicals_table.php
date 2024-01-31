ab3ee89314f5df955f9bff6f963e1ea183654593
Ivan <kulkov_2003@bk.ru>
2024-01-31 14:21:23 +0300

    Add KamjiRadical model with migrations

---
+++ database/migrations/2024_01_31_111501_create_kanji_radicals_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kanji_radicals', function (Blueprint $table) {
+            $table->unsignedBigInteger('kanji_id');
+            $table->unsignedBigInteger('radical_id');
+
+            $table->index('kanji_id', 'kanji_radical_kanji_idx');
+            $table->index('radical_id', 'kanji_radical_radical_idx');
+
+            $table->foreign('kanji_id', 'kanji_radical_kanji_fk')->on('kanji')->references('id');
+            $table->foreign('radical_id', 'kanji_radical_radical_fk')->on('radical')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kanji_radicals');
+    }
+};
