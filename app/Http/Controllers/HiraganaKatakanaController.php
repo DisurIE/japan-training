cd353363220af5ba391487266c04e01948b17666
Ivan <kulkov_2003@bk.ru>
2024-02-01 16:42:20 +0300

    add hiragana-katakana controller

---
+++ app/Http/Controllers/HiraganaKatakanaController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\Kanji;
+use App\Models\Radical;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class HiraganaKatakanaController extends Controller
+{
+    public function index(Radical $radicals)
+    {
+        return Inertia::render('Radicals', [
+            'radicals' => $radicals->all()
+        ]);
+    }
+
+    public function show(Radical $radical)
+    {
+        return Inertia::render('RadicalShow', [
+            'radical' => $radical
+        ]);
+    }
+}
