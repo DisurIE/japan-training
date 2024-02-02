a86b4fdbcbedd2c54edfc523900c766048dc3469
Ivan <kulkov_2003@bk.ru>
2024-02-02 20:40:08 +0300

    add radicals page

---
+++ resources/js/Pages/Radicals.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import NavLink from "@/Components/NavLink.vue";
+
+defineProps({
+    radicals: Array,
+});
+
+</script>
+
+<template>
+    Радикалы
+</template>
+
+<style>
+
+</style>
