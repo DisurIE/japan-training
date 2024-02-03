23715b9e0cce3c6abab00dccca41923ff756dee5
Ivan <kulkov_2003@bk.ru>
2024-02-03 10:59:46 +0300

    add change alphabets in hiragana-katakana

--- resources/js/Components/HiraganaKatakanaComponent.vue
+++ resources/js/Components/HiraganaKatakanaComponent.vue
@@ -3,8 +3,20 @@ export default {
     data() {
         return {
             isHiragana: true,
-            hiraganaCharacters: ['あ', 'い', 'う', 'え', 'お'],
-            katakanaCharacters: ['ア', 'イ', 'ウ', 'エ', 'オ'],
+            hiraganaCharacters: [
+                ['あ', 'い', 'う', 'え', 'お'],
+                ['あ', 'い', 'う', 'え', 'お'],
+                ['あ', 'い', 'う', 'え', 'お'],
+                ['あ', 'い', 'う', 'え', 'お'],
+                ['あ', 'い', 'う', 'え', 'お'],
+            ],
+            katakanaCharacters: [
+                ['ア', 'イ', 'ウ', 'エ', 'オ'],
+                ['ア', 'イ', 'ウ', 'エ', 'オ'],
+                ['ア', 'イ', 'ウ', 'エ', 'オ'],
+                ['ア', 'イ', 'ウ', 'エ', 'オ'],
+                ['ア', 'イ', 'ウ', 'エ', 'オ'],
+            ],
         };
     },
     computed: {
@@ -23,58 +35,16 @@ export default {
 
 <template>
     <div>
-        <h1>{{ currentAlphabet }}</h1>
+
 
         <div class="px-2 py-3 w-auto flex justify-center">
             <table class=" text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead>
-                <tr>
-                    <th class="text-black border-2 border-gray-100 text-center text-3xl p-1">あ</th>
-                    <th class="text-black border-2 border-gray-100 text-center  text-3xl p-1">か</th>
-                    <th class="text-black border-2 border-gray-100 text-center text-3xl p-1">さ</th>
-                    <th class="text-black border-2 border-gray-100 text-center text-3xl p-1">た</th>
-                    <th class="text-black border-2 border-gray-100 text-center text-3xl p-1">な</th>
-                    <th class="text-black border-2 border-gray-100 text-center text-3xl p-1">は</th>
-                    <th class="text-black border-2 border-gray-100 text-center text-3xl p-1">ま</th>
-                </tr>
-                </thead>
+
                 <tbody>
-                <tr>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
+                <tr v-for="row in currentAlphabet">
+                    <td v-for="character in row" class="text-black border-2 border-gray-100 text-center text-3xl p-1">{{character}}</td>
                 </tr>
-                <tr>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                </tr>
-                <tr>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                </tr>
-                <tr>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                    <td class="text-black border-2 border-gray-100 text-center text-3xl p-1">い</td>
-                </tr>
+
                 </tbody>
             </table>
         </div>
