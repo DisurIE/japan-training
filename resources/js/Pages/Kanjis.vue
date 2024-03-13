c8b44b3ada943387083b079e584213f24698c9e8
Ivan <kulkov_2003@bk.ru>
2024-03-13 21:24:17 +0300

    add groupd on levels in kanjis

--- resources/js/Pages/Kanjis.vue
+++ resources/js/Pages/Kanjis.vue
@@ -1,7 +1,23 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import NavLink from "@/Components/NavLink.vue";
+import HeaderComponent from "@/Components/HeaderComponent.vue";
+import { onMounted } from 'vue';
+
+
+
+</script>
+
 <script>
-import { defineProps } from 'vue';
+import {defineProps, onMounted} from 'vue';
 
 export default {
+    data() {
+        return {
+            levelArray: [],
+            counter : 0,
+        }
+    },
     props: {
         kanjis: Array
     },
@@ -12,44 +28,61 @@ export default {
             } else {
                 return text.slice(0, length) + '...';
             }
-        }
-    }
+        },
+
+    },
+    computed: {
+        getLevels: function () {
+            this.kanjis.forEach(kanji => {
+                this.levelArray.push(kanji.level);
+            })
+            return [...new Set(this.levelArray)];
+        },
+        kanjiGroup: function() {
+            const grouped = {};
+            this.kanjis.forEach(kanji => {
+                if (!grouped[kanji.level]) {
+                    grouped[kanji.level] = [];
+                }
+                grouped[kanji.level].push(kanji);
+            });
+            return grouped;
+        },
+    },
 }
 </script>
 
-<script setup>
-import { Head, Link } from '@inertiajs/vue3';
-import NavLink from "@/Components/NavLink.vue";
-import HeaderComponent from "@/Components/HeaderComponent.vue";
-</script>
 
 <template>
     <div>
         <HeaderComponent/>
         <Head title="Kanjis" />
         <div class="container mx-auto p-4">
-            <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
-                <div
-                    class="relative bg-blue-400 p-6 rounded-lg cursor-pointer overflow-hidden transition duration-300 transform hover:scale-105"
-                    v-for="kanji in kanjis"
-                    :key="kanji.id"
-                >
-                    <Link :href="route('kanjis.show', { id: kanji.id })">
-                        <div class="font-bold text-6xl text-center">{{ kanji.character }}</div>
-                        <div
-                            class="absolute inset-0 bg-gray-800 text-white opacity-0 transition duration-300 hover:opacity-100"
-                        >
-                            <div class="flex flex-col justify-center h-full px-4 py-2">
-                                <div class="mb-1 text-center text-lg">{{ kanji.onyomi }}</div>
-                                <div class="text-center text-lg">{{ truncate(kanji.meaning, 20) }}</div>
-                            </div>
+            <!-- Вывод кандзи для каждого уровня -->
+            <div v-for="(level, index) in getLevels" :key="index">
+                <h2>{{ level }}</h2>
+                <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
+                    <!-- Отображение кандзи для текущего уровня -->
+                    <div v-for="kanji in kanjiGroup[level]" :key="kanji.id">
+                        <div class="relative bg-blue-400 p-6 rounded-lg cursor-pointer overflow-hidden transition duration-300 transform hover:scale-105">
+                            <Link :href="route('kanjis.show', { id: kanji.id })">
+                                <div class="font-bold text-6xl text-center">{{ kanji.character }}</div>
+                                <div class="absolute inset-0 bg-gray-800 text-white opacity-0 transition duration-300 hover:opacity-100">
+                                    <div class="flex flex-col justify-center h-full px-4 py-2">
+                                        <div class="mb-1 text-center text-lg">{{ kanji.onyomi }}</div>
+                                        <div class="text-center text-lg">{{ truncate(kanji.meaning, 20) }}</div>
+                                        <div class="text-center text-lg">{{ kanji.level }}</div>
+                                    </div>
+                                </div>
+                            </Link>
                         </div>
-                    </Link>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
 
 <style>
 /* Стилизация для локального файла */
