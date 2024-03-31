dac23abbdd19ff58e4443c2816f57fab4266e84d
vboxuser <kulkov_2003@bk.ru>
2024-03-31 12:17:16 +0300

    refactor emial verification

--- app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Registered;
 
 class EmailVerificationNotificationController extends Controller
 {
---
+++ resources/js/Pages/Kanji/KanjisCreate.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import NavLink from "@/Components/NavLink.vue";
+import HeaderComponent from "@/Components/HeaderComponent.vue";
+
+defineProps({
+    kanji: Object,
+});
+
+</script>
+
+<template>
+    <Layout>
+        <Head title="Kanjis" />
+        <HeaderComponent/>
+        <div class="bg-blue-400 flex-auto p-2.5 max-w-auto p-7">
+            <div class="font-bold text-7xl" >
+                {{ kanji.character }}
+            </div>
+            <div class="text-2xl">{{ kanji.meaning }}</div>
+            <div class="text-2xl">onyomi: {{ kanji.onyomi }}</div>
+            <div class="text-2xl">kunyomi: {{ kanji.kunyomi }}</div>
+            <div class="text-2xl">Level: {{ kanji.level }}</div>
+        </div>
+
+    </Layout>
+</template>
+
+<style>
+
+</style>
