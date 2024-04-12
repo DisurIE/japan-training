761ed3eb2d3a20a6d4974078fb299545dc6a0617
vboxuser <kulkov_2003@bk.ru>
2024-04-12 14:10:51 +0300

    add dark theme on radicals page

--- resources/js/Pages/Radicals.vue
+++ resources/js/Pages/Radicals.vue
@@ -2,6 +2,7 @@
 import { Head, Link } from '@inertiajs/vue3';
 import NavLink from "@/Components/NavLink.vue";
 import HeaderComponent from "@/Components/HeaderComponent.vue";
+import GuestLayout from "@/Layouts/GuestLayout.vue";
 
 
 </script>
@@ -28,26 +29,28 @@ export default {
 <template>
     <HeaderComponent/>
     <Head title="Radicals" />
-    <div class="container mx-auto p-4">
+    <GuestLayout>
+        <div class="container mx-auto p-4">
 
-            <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
-                <!-- Отображение ключей -->
-                <div v-for="radical in radicals" :key="radical.id">
-                    <div class="relative bg-green-400 p-6 rounded-lg cursor-pointer overflow-hidden transition duration-300 transform hover:scale-105">
-                        <Link :href="route('radicals.show', { id: radical.character })">
-                            <div class="font-bold text-6xl text-center">{{ truncate(radical.character, 1) }}</div>
-                            <div class="absolute inset-0 bg-gray-800 text-white opacity-0 transition duration-300 hover:opacity-100">
-                                <div class="flex flex-col justify-center h-full px-4 py-2">
-                                    <div class="text-center text-lg">{{ execute(radical.meaning, ' ') }}</div>
-                                    <div class="mb-1 text-center text-lg">{{ execute(radical.reading, ' ')}}</div>
-                                    <div class="text-center text-lg">{{ radical.strokes }}</div>
+                <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
+                    <!-- Отображение ключей -->
+                    <div v-for="radical in radicals" :key="radical.id">
+                        <div class="relative bg-green-400 dark:bg-slate-500 p-6 rounded-lg cursor-pointer overflow-hidden transition duration-300 transform hover:scale-105">
+                            <Link :href="route('radicals.show', { id: radical.character })">
+                                <div class="font-bold text-6xl text-center">{{ truncate(radical.character, 1) }}</div>
+                                <div class="absolute inset-0 bg-gray-800 dark:bg-slate-700 text-white opacity-0 transition duration-300 hover:opacity-100">
+                                    <div class="flex flex-col justify-center h-full px-4 py-2">
+                                        <div class="text-center text-lg">{{ execute(radical.meaning, ' ') }}</div>
+                                        <div class="mb-1 text-center text-lg">{{ execute(radical.reading, ' ')}}</div>
+                                        <div class="text-center text-lg">{{ radical.strokes }}</div>
+                                    </div>
                                 </div>
-                            </div>
-                        </Link>
+                            </Link>
+                        </div>
                     </div>
                 </div>
-            </div>
-    </div>
+        </div>
+    </GuestLayout>
 </template>
 
 <style scoped>
