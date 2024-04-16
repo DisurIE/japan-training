0a2a391c5abd2438955553e4e76c59b00252d096
vboxuser <kulkov_2003@bk.ru>
2024-04-16 15:44:50 +0300

    add create controller and route

---
+++ app/Http/Controllers/Radical/CreateController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Radical;
+
+use App\Helpers\JsonHandler;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\Kanji;
+use App\Models\Radical;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class CreateController extends Controller
+{
+    public function __invoke()
+    {
+        return Inertia::render('Kanji/KanjisCreate');
+    }
+}
