67d4e6d0391eb9468bc980438b774ab976474dc0
Ivan Kulkov <kulkov_2003@bk.ru>
2024-05-22 07:03:01 -0400

    refactor

--- resources/js/Pages/DashboardKanjis.vue
+++ resources/js/Pages/DashboardKanjis.vue
@@ -15,7 +15,7 @@ export default {
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Уровни</h2>
         </template>
 
         <div class="py-12">
